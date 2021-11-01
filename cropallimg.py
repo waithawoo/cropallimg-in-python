9f7a2676fbbf762022da3f7c5f27859ec9bcc035
Wai Thaw Oo <42320296+waithawoo@users.noreply.github.com>
2021-11-01 15:07:35 +0630

    Update cropallimg.py

--- cropallimg.py
+++ cropallimg.py
@@ -1,34 +1,53 @@
 import os
-import glob #pip install glob2
-import re
-import numpy as np
-from PIL import Image #pip install Pillow
-from pyfiglet import Figlet #pip install pyfiglet
+import glob  # pip install glob2
+from PIL import Image  # pip install Pillow
+from pyfiglet import Figlet  # pip install pyfiglet
 
 try:
     custom_fig = Figlet(font='standard')
 
-    print("Please enter Your Images Path.\n\nPlease use Path Name like this (with double \\\) \n\neg:D:\\\Download\\\jaffedbase\n\n########################################")
+    def getDirandFiletype():
+        global img_dir, outpath, filenames
 
-    img_dir = input("What is your image directory path : ")
-    outpath = input("What is your output path where to be saved : ")
-    filetype = input("Enter file type : ")
+        print("Please enter Your Images Path.\n\neg:D:\Download\jaffedbase\n\n########################################")
+        img_dir = input("What is your image directory path : ")
+        outpath = input("What is your output path where to be saved : ")
+        while(outpath == img_dir):
+            print('Enter a different path with the image directory!!')
+            outpath = input("What is your output path where to be saved : ")
 
-    print("Recommended for jaffedbase : x1=50, y1=50, x2=190, y2=235\n")
-    x1_coordinate = int(input("Enter x1 coordinate : "))#50
-    y1_coordinate = int(input("Enter y1 coordinate : "))#50
-    x2_coordinate = int(input("Enter x2 coordinate : "))#190
-    y2_coordinate = int(input("Enter y2 coordinate : "))#235
-    data_path = os.path.join(img_dir)
-    filenames = glob.glob(data_path + "/*." + filetype) #read all files in the path mentioned
+        filetype = input("Enter file type (eg: png) : ")
+        data_path = os.path.join(img_dir)
+        # read all files in the path mentioned
+        filenames = glob.glob(data_path + "/*." + filetype)
+        processed = 0
+        for file in filenames:
+            img = Image.open(str(file))
+            width, height = img.size
+            print("Dimension of source images : "+width+"x"+height)
+            processed += 1
+            if processed >= 1:
+                break 
 
-    for x in filenames:
-        name = (x.replace(img_dir, ''))
-        img = Image.open(str(x))
-        region = img.crop((x1_coordinate, y1_coordinate, x2_coordinate, y2_coordinate))
-        region.save(outpath  + name)
-    print(custom_fig.renderText('Done Image Cropping!'))
+        
+    def getCoordinate():
+        global left, top, right, bottom, img
+        left = int(input("Enter left coordinate : "))  # 50
+        top = int(input("Enter top coordinate : "))  # 50
+        right = int(input("Enter right coordinate : "))  # 190
+        bottom = int(input("Enter bottom coordinate : "))  # 235
+    
+    def main():
+        getDirandFiletype()
+        getCoordinate()
+        for x in filenames:
+            name = x.replace(img_dir, '')
+            img = Image.open(str(x))
+            region = img.crop((left, top,right, bottom))
+            region.save(outpath + name)
+        print(custom_fig.renderText('Done Image Cropping!'))
+    main()
 except SystemError:
-        print("Sorry Buddy. Plese Check Coordinates Again!")
+    print("Sorry Buddy. Your Coordinates are invalid. Plese Check Coordinates Again!")
 except ValueError:
-        print("Please use Only Numbers for Coordinates!")
+    print("Please use Only Numbers for Coordinates!")
